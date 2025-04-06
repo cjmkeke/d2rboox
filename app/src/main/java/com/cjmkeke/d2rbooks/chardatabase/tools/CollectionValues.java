5646c254cc4c8ba7264ad15d68c0347b75baed49
cjmkeke <cjmkeke@naver.com>
2025-04-06 19:55:16 +0900

    Merge remote-tracking branch 'github/master'

---
+++ app/src/main/java/com/cjmkeke/d2rbooks/chardatabase/tools/CollectionValues.java
@@ -0,0 +1,8 @@
+package com.cjmkeke.d2rbooks.chardatabase.tools;
+
+public class CollectionValues {
+
+    public static final int DEFAULT_SKILL_VALUE = 1000; // 기본값 98
+    public static final int DEFAULT_SKILL_POINT = 20; // 기본값 0
+
+}
---
+++ app/src/main/java/com/cjmkeke/d2rbooks/chardatabase/tools/JsonModels.java
@@ -0,0 +1,100 @@
+package com.cjmkeke.d2rbooks.chardatabase.tools;
+
+public class JsonModels {
+
+    private String option1;
+    private String option2;
+    private String option3;
+    private String option4;
+    private String option5;
+    private String option6;
+    private String option7;
+    private String option8;
+    private String option9;
+    private String option10;
+    private String radius;
+    private String damage;
+    private String damage1;
+    private String damage2;
+    private String attack_rating1;
+    private String attack_rating2;
+    private String cold_damage_added_to_player;
+    private String aura_cold_damage;
+    private String ememies_slowed;
+
+    public String getCold_damage_added_to_player() {
+        return cold_damage_added_to_player;
+    }
+
+    public String getAura_cold_damage() {
+        return aura_cold_damage;
+    }
+
+    public String getEmemies_slowed() {
+        return ememies_slowed;
+    }
+
+    public String getAttack_rating1() {
+        return attack_rating1;
+    }
+
+    public String getAttack_rating2() {
+        return attack_rating2;
+    }
+
+    public String getDamage1() {
+        return damage1;
+    }
+
+    public String getDamage2() {
+        return damage2;
+    }
+
+    public String getDamage() {
+        return damage;
+    }
+
+    public String getRadius() {
+        return radius;
+    }
+
+    public String getOption1() {
+        return option1;
+    }
+
+    public String getOption2() {
+        return option2;
+    }
+
+    public String getOption3() {
+        return option3;
+    }
+
+    public String getOption4() {
+        return option4;
+    }
+
+    public String getOption5() {
+        return option5;
+    }
+
+    public String getOption6() {
+        return option6;
+    }
+
+    public String getOption7() {
+        return option7;
+    }
+
+    public String getOption8() {
+        return option8;
+    }
+
+    public String getOption9() {
+        return option9;
+    }
+
+    public String getOption10() {
+        return option10;
+    }
+}
